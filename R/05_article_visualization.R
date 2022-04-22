fa03b96623397616582b424493227954d0762b98
JoachimBreitenstein <joachim.breitenstein@hotmail.com>
2022-04-22 13:29:40 +0000

    Article plot (corrected)

--- R/05_article_visualization.R
+++ R/05_article_visualization.R
@@ -3,14 +3,15 @@ data <- read_csv(file = "data/02_dat_clean.csv")
 
 #augmenting data to fit plot
 data <- data %>% 
-  mutate(Groups = case_when(Group == 1 ~ "PCa cases",
+  select(Group, mtDNA) %>% 
+  mutate(Group = case_when(Group == 1 ~ "PCa cases",
                             Group == 0 ~ "control"))
 
 #recreating plot from article
 ggplot(data,
-       mapping = aes(x = factor(Groups),
+       mapping = aes(x = Group,
                      y = mtDNA,
-                     stratification = Groups)) + 
+                     stratification = Group)) + 
   geom_boxplot() 
   
   
