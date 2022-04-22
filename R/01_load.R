c898536392cbb5e2fbbf1dd9fd18e265d40197a0
WilliamHR <s194545@dtu.dk>
2022-04-22 08:19:26 +0000

    Create 00_doit, rename "scripts" dictory to "R"

--- R/01_load.R
+++ R/01_load.R
@@ -1,15 +1,10 @@
-library(tidyverse)
-library(readxl)
-
 #Load data sheet
-data_raw <- read_excel("data/_raw/Data.xlsx",
+read_excel("data/_raw/Data.xlsx",
                    sheet = 2) %>% 
   write_csv("data/01_dat_load.csv")
 
 #Load legend sheet
-legend <- read_excel("data/_raw/Data.xlsx",
+read_excel("data/_raw/Data.xlsx",
                    sheet = 1) %>%
   write_csv("data/01_legend_load.csv")
-
-
 
