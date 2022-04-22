7ff76070d57897047d9c25e49e890a4dc808740d
Fiskild <s184241@dtu.dk>
2022-04-22 09:56:12 +0000

    update cleaning

--- R/01_load.R
+++ R/01_load.R
@@ -5,12 +5,11 @@ library(tidyverse)
 # set to "custom" in the data/_raw/Data.xlsx and not "general"
 # the data is loaded as a date and time and not a number.
 # This has forced us to indicate each class of data seperately 
-data_raw <- read_excel("data/_raw/Data.xlsx",
-                       col_types = c("text","numeric","numeric","numeric",
-                                     "text","numeric","numeric","numeric",
-                                     "numeric","numeric","numeric",
-                                     "numeric","numeric"),
-                       sheet = 2) %>% 
+read_excel("data/_raw/Data.xlsx",
+           col_types = c("text","numeric","numeric","numeric","text",
+                         "numeric","numeric","numeric","numeric",
+                         "numeric","numeric","numeric","numeric"),
+           sheet = 2) %>%
   write_csv("data/01_dat_load.csv")
 
 #Load legend sheet
--- R/02_clean.R
+++ R/02_clean.R
@@ -1,3 +1,6 @@
-pcr = read_csv("data/01_dat_load") %>% 
-  distinct(PCRsucces, .keepall())
+library(tidyverse)
 
+read_csv("data/01_dat_load.csv") %>% 
+  filter(PCRsuccess == 1) %>% 
+  mutate()
+  write_csv("data/02_dat_clean.csv")
