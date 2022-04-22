4d1123662f3affefa6f6e38171c693fe5f859ef0
Fiskild <s184241@dtu.dk>
2022-04-22 10:08:03 +0000

    Better naming convention incorperated in load

--- R/01_load.R
+++ R/01_load.R
@@ -5,11 +5,12 @@ library(tidyverse)
 # set to "custom" in the data/_raw/Data.xlsx and not "general"
 # the data is loaded as a date and time and not a number.
 # This has forced us to indicate each class of data seperately 
-read_excel("data/_raw/Data.xlsx",
+john = read_excel("data/_raw/Data.xlsx",
+           sheet = 2,
            col_types = c("text","numeric","numeric","numeric","text",
                          "numeric","numeric","numeric","numeric",
                          "numeric","numeric","numeric","numeric"),
-           sheet = 2) %>%
+           .name_repair = "universal") %>%
   write_csv("data/01_dat_load.csv")
 
 #Load legend sheet
