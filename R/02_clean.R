a67fbfb9175572c60252ee25f2d4bfc3f33d52fc
EsbenOyan <s194564@dtu.dk>
2022-04-24 09:13:15 +0000

    Played around with error messages. Contact TA.

--- R/02_clean.R
+++ R/02_clean.R
@@ -1,15 +1,14 @@
 library(tidyverse)
 
 filtered_data <- read_csv("data/01_dat_load.csv") %>% 
-  filter(PCRsuccess == 1) 
+  filter(PCRsuccess == 1)
+  # add_row(Sample = "CM4033")
 
-is_duplicated <- filtered_data %>% 
-  group_by(Sample) %>% 
-  filter(n() != 1) %>% 
-  ungroup() %>% 
-  pull()
-print(str("Are there any duplications in the dataset?", is_duplicated))
-
+# is_duplicated <- filtered_data %>% 
+#   group_by(Sample) %>% 
+#   filter(n() > 1) %>% 
+#   nrow()
+# if_else(is_duplicated > 0 ~ print("ERROR: Duplicates in dataset"))
 
 filtered_data %>% 
   write_csv("data/02_dat_clean.csv")
