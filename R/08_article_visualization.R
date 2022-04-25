ee9637bdedb98b79cc7b7428411aa123c56de2c9
JoachimBreitenstein <joachim.breitenstein@hotmail.com>
2022-04-25 09:50:50 +0000

    Correlation analysis script

--- R/08_article_visualization.R
+++ R/08_article_visualization.R
@@ -13,15 +13,18 @@ ggplot(data,
                      y = mtDNA,
                      stratification = Group, 
                      fill  = Group)) + 
-  stat_boxplot(geom = "errorbar", width = 0.5) +  
+  #stat_boxplot(geom = "errorbar", width = 0.5) +  
   geom_boxplot(outlier.shape = 1) +
   theme_classic() +
-  theme(legend.position = "right") +
-  labs(title = "Plot", 
+  theme(legend.position = "right") + 
+  labs(title = "Boxplot of relative mtDNA copy number of controls and PCa cases", 
        y = "mtDNA", 
        x = "Group", 
-       caption = "Figure: Distribution of pheripheral blood mtDNA copy number in Han chinese with prostate cancer and healthy controls.") 
-      
+       caption = "Figure: Distribution of pheripheral blood mtDNA copy number in Han chinese with prostate cancer and healthy controls.") +
+  theme(plot.title = element_text(size = 10), plot.caption = element_text(size = 5))
+
+#performing t.test of the groups
+data %>% split(f = "Group")
     
   
 
