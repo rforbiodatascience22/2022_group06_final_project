8c4a41eb79b3b0478bada7c90361428bbfa4b39e
Fiskild <s184241@dtu.dk>
2022-04-25 09:29:40 +0000

    Better plot for outliers, now stratisfied on Group_names

--- R/04_test_for_outliers.R
+++ R/04_test_for_outliers.R
@@ -9,104 +9,189 @@ data <- read_csv("data/03_dat_aug.csv")
 
 # Create boxplot for each numeric variable to check for outliers ----------
 AgePlot <- data %>% 
-  ggplot(mapping = aes(y = Age, x = "")) +
-  geom_boxplot() + 
-  geom_point(position = position_jitter(w = 0.1, h = 0.2),
-             alpha = 0.3) + 
-  stat_boxplot(geom = "errorbar", width = 0.5) + 
+  ggplot(aes(y = Age,
+             x = Group_names,
+             color = Group_names)) +
+  geom_boxplot(outlier.shape = NA,
+               show.legend = FALSE) + 
+  geom_jitter(show.legend = FALSE,
+              alpha = 0.5,
+              width = 0.15) + 
+  stat_boxplot(geom = "errorbar",
+               width = 0.5,
+               show.legend = FALSE) + 
   labs(x = "Age",
        y = "") + 
+  scale_color_brewer(palette = "Dark2") +
   theme_classic()
+
 
 FatIntakePlot <- data %>% 
-  ggplot(mapping = aes(y = Dfi, x = "")) +
-  geom_boxplot() + 
-  geom_point(position = position_jitter(w = 0.1, h = 0.2),
-             alpha = 0.3) + 
-  stat_boxplot(geom = "errorbar", width = 0.5) + 
+  ggplot(aes(y = Dfi,
+             x = Group_names,
+             color = Group_names)) +
+  geom_boxplot(outlier.shape = NA,
+               show.legend = FALSE) + 
+  geom_jitter(show.legend = FALSE,
+              alpha = 0.5,
+              width = 0.15) + 
+  stat_boxplot(geom = "errorbar",
+               width = 0.5,
+               show.legend = FALSE) + 
   labs(x = "Daily dietary fat intake",
        y = "") + 
+  scale_color_brewer(palette = "Dark2") +
   theme_classic()
+
 
 BMIPlot <- data %>% 
-  ggplot(mapping = aes(y = BMI, x = "")) +
-  geom_boxplot() + 
-  geom_point(position = position_jitter(w = 0.1, h = 0.2),
-             alpha = 0.3) + 
-  stat_boxplot(geom = "errorbar", width = 0.5) + 
+  ggplot(aes(y = BMI,
+             x = Group_names,
+             color = Group_names)) +
+  geom_boxplot(outlier.shape = NA,
+               show.legend = FALSE) + 
+  geom_jitter(show.legend = FALSE,
+              alpha = 0.5,
+              width = 0.15) + 
+  stat_boxplot(geom = "errorbar",
+               width = 0.5,
+               show.legend = FALSE) + 
   labs(x = "BMI",
        y = "") + 
+  scale_color_brewer(palette = "Dark2") +
   theme_classic()
-  
-AJCCPlot <- data %>% 
-  ggplot(mapping = aes(y = AJCC, x = "")) +
-  geom_boxplot() + 
-  geom_point(position = position_jitter(w = 0.1, h = 0.2),
-             alpha = 0.3) + 
-  stat_boxplot(geom = "errorbar", width = 0.5) + 
-  labs(x = "AJJC stage",
-       y = "") + 
-  theme_classic()
+
 
 PSAPlot <- data %>% 
-  ggplot(mapping = aes(y = PSA, x = "")) +
-  geom_boxplot() + 
-  geom_point(position = position_jitter(w = 0.1, h = 0.2),
-             alpha = 0.3) + 
-  stat_boxplot(geom = "errorbar", width = 0.5) + 
+  ggplot(aes(y = PSA,
+             x = Group_names,
+             color = Group_names)) +
+  geom_boxplot(outlier.shape = NA,
+               show.legend = FALSE) + 
+  geom_jitter(show.legend = FALSE,
+              alpha = 0.5,
+              width = 0.15) + 
+  stat_boxplot(geom = "errorbar",
+               width = 0.5,
+               show.legend = FALSE) + 
   labs(x = "Prostate specific antigen",
        y = "") + 
+  scale_color_brewer(palette = "Dark2") +
   theme_classic()
 
-GleasonPlot <- data %>% 
-  ggplot(mapping = aes(y = Gleason, x = "")) +
-  geom_boxplot() + 
-  geom_point(position = position_jitter(w = 0.1, h = 0.2),
-             alpha = 0.3) + 
-  stat_boxplot(geom = "errorbar", width = 0.5) + 
-  labs(x = "Gleason score",
-       y = "") + 
-  theme_classic()
 
 mtDNAPlot <- data %>% 
-  ggplot(mapping = aes(y = mtDNA, x = "")) +
-  geom_boxplot() + 
-  geom_point(position = position_jitter(w = 0.1, h = 0.2),
-             alpha = 0.3) + 
-  stat_boxplot(geom = "errorbar", width = 0.5) + 
+  ggplot(aes(y = mtDNA,
+             x = Group_names,
+             color = Group_names)) +
+  geom_boxplot(outlier.shape = NA,
+               show.legend = FALSE) + 
+  geom_jitter(show.legend = FALSE,
+              alpha = 0.5,
+              width = 0.15) + 
+  stat_boxplot(geom = "errorbar",
+               width = 0.5,
+               show.legend = FALSE) + 
   labs(x = "mtDNA copy number",
        y = "") + 
+  scale_color_brewer(palette = "Dark2") +
   theme_classic()
+
+
+GleasonPlot <- data %>% 
+  ggplot(aes(y = Gleason,
+             x = "",
+             color = Group_names)) +
+  geom_boxplot(outlier.shape = NA,
+               show.legend = FALSE) + 
+  geom_jitter(show.legend = FALSE,
+              alpha = 0.5,
+              width = 0.2,
+              height = 0.2) + 
+  stat_boxplot(geom = "errorbar",
+               width = 0.5,
+               show.legend = FALSE) + 
+  labs(x = "Gleason score",
+       y = "") + 
+  scale_color_brewer(palette = "Dark2") +
+  theme_classic()
+
+
+AJCCPlot <- data %>% 
+  ggplot(aes(y = AJCC,
+             x = "",
+             color = Group_names)) +
+  geom_boxplot(outlier.shape = NA,
+               show.legend = FALSE) + 
+  geom_jitter(show.legend = FALSE,
+              alpha = 0.5,
+              width = 0.2,
+              height = 0.2) + 
+  stat_boxplot(geom = "errorbar",
+               width = 0.5,
+               show.legend = FALSE) + 
+  labs(x = "AJCC Stage",
+       y = "") + 
+  scale_color_brewer(palette = "Dark2") +
+  theme_classic()  
+
 
 TumorPlot <- data %>% 
-  ggplot(mapping = aes(y = Tumor, x = "")) +
-  geom_boxplot() + 
-  geom_point(position = position_jitter(w = 0.1, h = 0.2),
-             alpha = 0.3) + 
-  stat_boxplot(geom = "errorbar", width = 0.5) + 
+  ggplot(aes(y = Tumor,
+             x = "",
+             color = Group_names)) +
+  geom_boxplot(outlier.shape = NA,
+               show.legend = FALSE) + 
+  geom_jitter(show.legend = FALSE,
+              alpha = 0.5,
+              width = 0.2,
+              height = 0.2) + 
+  stat_boxplot(geom = "errorbar",
+               width = 0.5,
+               show.legend = FALSE) + 
   labs(x = "Tumor severity",
        y = "") + 
+  scale_color_brewer(palette = "Dark2") +
   theme_classic()
+
 
 NodesPlot <- data %>% 
-  ggplot(mapping = aes(y = LymphNodes, x = "")) +
-  geom_boxplot() + 
-  geom_point(position = position_jitter(w = 0.1, h = 0.2),
-             alpha = 0.3) + 
-  stat_boxplot(geom = "errorbar", width = 0.5) + 
+  ggplot(aes(y = LymphNodes,
+             x = "",
+             color = Group_names)) +
+  geom_boxplot(outlier.shape = NA,
+               show.legend = FALSE) + 
+  geom_jitter(show.legend = FALSE,
+              alpha = 0.5,
+              width = 0.2,
+              height = 0.2) + 
+  stat_boxplot(geom = "errorbar",
+               width = 0.5,
+               show.legend = FALSE) + 
   labs(x = "Lymph nodes affected",
        y = "") + 
+  scale_color_brewer(palette = "Dark2") +
   theme_classic()
 
+
 MetastasisPlot <- data %>% 
-  ggplot(mapping = aes(y = Metastasis, x = "")) +
-  geom_boxplot() + 
-  geom_point(position = position_jitter(w = 0.1, h = 0.2),
-             alpha = 0.3) + 
-  stat_boxplot(geom = "errorbar", width = 0.5) + 
+  ggplot(aes(y = Metastasis,
+             x = "",
+             color = Group_names)) +
+  geom_boxplot(outlier.shape = NA,
+               show.legend = FALSE) + 
+  geom_jitter(show.legend = FALSE,
+              alpha = 0.5,
+              width = 0.2,
+              height = 0.2) + 
+  stat_boxplot(geom = "errorbar",
+               width = 0.5) + 
   labs(x = "Metastasis",
-       y = "") + 
+       y = "",
+       color="Group names") + 
+  scale_color_brewer(palette = "Dark2") +
   theme_classic()
+
 
 Plot1 <- AgePlot + FatIntakePlot + PSAPlot + BMIPlot + mtDNAPlot +
   plot_layout(ncol = 5) + 
@@ -125,3 +210,4 @@ ggsave(filename = 'results/OutlierPlot2.png',
        plot = Plot2,
        width = 10,
        height = 6.5)
+Plot2
