55789388d988580d916deadc7c9afdfd622f5eee
JoachimBreitenstein <joachim.breitenstein@hotmail.com>
2022-04-29 10:48:00 +0000

    boxplots of PSA for controls and PCa

--- R/08_article_visualization.R
+++ R/08_article_visualization.R
@@ -5,7 +5,7 @@ library(ggpubr)
 data <- read_csv(file = "data/03_dat_aug.csv")
 
 #Recreating plot from article
-plt <- ggplot(data, mapping = aes(x = Group_names,
+plt1 <- ggplot(data, mapping = aes(x = Group_names,
                            y = mtDNA,
                            stratification = Group_names, 
                            fill  = Group_names)) + 
@@ -24,9 +24,27 @@ plt <- ggplot(data, mapping = aes(x = Group_names,
         plot.caption.position = "plot") + 
   ylim(0, 2.8)
 
-plt + ggpubr::stat_compare_means(method = "t.test", size = 3)
+plt1 + ggpubr::stat_compare_means(method = "t.test", size = 3)
 
 
-    
+plt2 <- ggplot(data, mapping = aes(x = Group_names,
+                                  y = PSA,
+                                  stratification = Group_names, 
+                                  fill  = Group_names)) + 
+  
+  stat_boxplot(geom = "errorbar", width = 0.5) +  
+  geom_boxplot(outlier.shape = 1) +
+  geom_signif(comparisons = list(c("Controls", "PCa cases")), 
+              map_signif_level=TRUE, size = 0.4) +
+  theme(legend.position = "right") + 
+  labs(title = "Boxplot of PSA of controls and PCa cases", 
+       y = "PSA", 
+       x = "Group", 
+       caption = "Figure: Distribution of pheripheral blood mtDNA copy number in Han chinese with prostate cancer and healthy controls.") +
+  theme(plot.title = element_text(size = 10), 
+        plot.caption = element_text(size = 6),
+        plot.caption.position = "plot")  
+
+plt2 + ggpubr::stat_compare_means(method = "t.test", size = 3)
 
 
