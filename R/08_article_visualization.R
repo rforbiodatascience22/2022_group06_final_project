505aff0d45eebdb1a52f1fd274f5435f7a2e8aab
Fiskild <s184241@dtu.dk>
2022-05-02 09:40:37 +0000

    Update visual

--- R/08_article_visualization.R
+++ R/08_article_visualization.R
@@ -19,14 +19,14 @@ article_plot <- ggplot(data, mapping = aes(x = group_names,
               map_signif_level = TRUE,
               size = 1) +
   theme(legend.position = "None") + 
-  labs(title = "Boxplot of relative mtDNA copy number
-                of controls and PCa cases", 
+  labs(title = "Boxplot of relative mtDNA copy number of controls and PCa cases", 
        y = "mtDNA", 
        x = "Group", 
        caption = "Figure: Distribution of pheripheral blood mtDNA copy number in 
 Han chinese with prostate cancer and healthy controls.") +
   theme(plot.caption = element_text(size = 22,
-                            hjust = 0)) + 
+                            hjust = 0),
+        text = element_text(size = 18)) + 
   ylim(0, 2.8)
 
 #Add statistical test
