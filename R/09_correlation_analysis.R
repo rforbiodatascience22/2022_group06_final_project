f6d37a306f6d980b6850167440075636855bdfe1
WilliamHR <s194545@dtu.dk>
2022-05-03 15:06:40 +0000

    fixed error in 09

--- R/09_correlation_analysis.R
+++ R/09_correlation_analysis.R
@@ -8,51 +8,51 @@ data <- read_csv(file = "data/03_dat_aug.csv",
 
 #split data frame
 data_split <- data %>% 
-  group_split(Group)
+  group_split(group)
 
 data_control <- as.data.frame(data_split[1])
-data_PCa <- as.data.frame(data_split[2])
+data_pca <- as.data.frame(data_split[2])
 
 #mtDNA vs. Age for controls  
-plt1 <- ggplot(data_control[[1]][[1]], mapping = aes(x = Age, 
-                                                     y = mtDNA)) + 
+plt1 <- ggplot(data_control[[1]][[1]], mapping = aes(x = age, 
+                                                     y = mtdna)) + 
   geom_point() 
 
 plt1+ ggpubr::stat_cor(method = "spearman")
 
 #mtDNA vs. Age for PCa cases
-plt2 <- ggplot(data_PCa[[1]][[1]], mapping = aes(x = Age, 
-                                                 y = mtDNA)) + 
+plt2 <- ggplot(data_PCa[[1]][[1]], mapping = aes(x = age, 
+                                                 y = mtdna)) + 
   geom_point()  
 
 plt2 + ggpubr::stat_cor(method = "spearman")
 
 
 #mtDNA vs. BMI for controls
-plt3 <- ggplot(data_control[[1]][[1]], mapping = aes(x = BMI, 
-                                                     y = mtDNA)) + 
+plt3 <- ggplot(data_control[[1]][[1]], mapping = aes(x = bmi, 
+                                                     y = mtdna)) + 
   geom_point()  
 
 plt3 + ggpubr::stat_cor(method = "spearman")
 
 
 #mtDNA vs. BMI for PCa cases
-plt4 <- ggplot(data_PCa[[1]][[1]], mapping = aes(x = BMI, 
-                                                 y = mtDNA)) + 
+plt4 <- ggplot(data_PCa[[1]][[1]], mapping = aes(x = bmi, 
+                                                 y = mtdna)) + 
   geom_point()  
 
 plt4 + ggpubr::stat_cor(method = "spearman")
 
 #mtDNA vs. gleason score
-plt5 <- ggplot(data_PCa[[1]][[1]], mapping = aes(x = Gleason, 
-                                                 y = mtDNA,
-                                                 color = Gleason)) + 
+plt5 <- ggplot(data_pca[[1]][[1]], mapping = aes(x = gleason, 
+                                                 y = mtdna,
+                                                 color = gleason)) + 
   geom_point()   
 
 plt5 + ggpubr::stat_cor(method = "spearman")
 
 #PSA vs. Gleason
-plt6 <- ggplot(data_PCa[[1]][[1]], mapping = aes(x = Gleason, 
-                                                 y = PSA)) + 
+plt6 <- ggplot(data_pca[[1]][[1]], mapping = aes(x = gleason, 
+                                                 y = psa)) + 
   geom_point()   
 plt6 + ggpubr::stat_cor(method = "spearman")
