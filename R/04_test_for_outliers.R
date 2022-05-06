671d0df69c7c4afca54e737ac5dfb85c056772f3
WilliamHR <s194545@dtu.dk>
2022-05-06 13:11:10 +0000

    changes to slide

--- R/04_test_for_outliers.R
+++ R/04_test_for_outliers.R
@@ -53,12 +53,12 @@ boxplot_discrete <- ajcc_plot +
   plot_annotation(title = "A plot of the discrete attributes, to examine distribution of outliers")
   
 #Save plots to results
-ggsave(filename = 'results/boxplot_continuous.png',
+ggsave(filename = "results/boxplot_continuous.png",
        plot = boxplot_continuous,
        width = 10,
        height = 6.5)
 
-ggsave(filename = 'results/boxplot_discrete.png',
+ggsave(filename = "results/boxplot_discrete.png",
        plot = boxplot_discrete,
        width = 10,
        height = 6.5)
