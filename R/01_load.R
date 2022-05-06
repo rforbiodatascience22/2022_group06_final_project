bcb084ba46711018aa14d1b222500ed205c94c4f
WilliamHR <s194545@dtu.dk>
2022-05-06 07:06:58 +0000

    Added slides

--- R/01_load.R
+++ R/01_load.R
@@ -19,7 +19,7 @@ cnames <- ifelse(str_detect(string = nms, pattern = "^PSA"), "numeric", "guess")
 # Load data set -----------------------------------------------------------
 # In addition to loading the data naming is changed to snake_case
 # Uses 'cnames' to decide columntypes of excel-file.
-read_excel(path = "data/_raw/Data.xlsx",
+readxl::read_excel(path = "data/_raw/Data.xlsx",
            sheet = "Data Set",
            col_types = cnames,
            .name_repair = "universal") %>%
@@ -39,7 +39,7 @@ read_excel(path = "data/_raw/Data.xlsx",
   write_csv(path = "data/01_dat_load.csv")
 
 # Load legend data --------------------------------------------------------
-read_excel(path = "data/_raw/Data.xlsx",
+readxl::read_excel(path = "data/_raw/Data.xlsx",
            sheet = "Column Legend") %>%
   write_csv(path = "data/01_legend_load.csv")
 
